e1848df35b16d6873a63968b361373590f66e165
PlasticDigits <plasticdigits@protonmail.com>
2022-01-13 05:01:45 -0800

    Add Rabbit Breed tests

--- contracts/RabbitBreed.sol
+++ contracts/RabbitBreed.sol
@@ -8,21 +8,29 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/PullPayment.sol";
 
 contract RabbitBreed is Ownable, EIP712MetaTransaction, PullPayment {
-
     RabbitRocket rabbitRocket;
 
-    constructor(RabbitRocket _rabbitRocket) EIP712MetaTransaction("@RabbitCatch/RabbitBreed","1.0.0") Ownable() {
-        rabbitRocket = _rabbitRocket;
+    constructor(RabbitRocket _rabbitRocket)
+        EIP712MetaTransaction("@RabbitCatch/RabbitBreed", "1.0.0")
+        Ownable()
+    {
+        setRabbitRocket(_rabbitRocket);
     }
 
     function addToRewards() external payable {}
 
-    function sendRewards(address _first, address _second, address _third) external onlyOwner {
-        require(rabbitRocket.isOver(),"RabbitGreed: RabbitRocket Is Not Over");
+    function sendRewards(
+        address _first,
+        address _second,
+        address _third
+    ) external onlyOwner {
+        require(rabbitRocket.isOver(), "RabbitGreed: RabbitRocket Is Not Over");
         _asyncTransfer(_first, address(this).balance / 2);
         _asyncTransfer(_second, address(this).balance / 2);
         _asyncTransfer(_third, address(this).balance);
     }
 
-
+    function setRabbitRocket(RabbitRocket _to) public onlyOwner {
+        rabbitRocket = _to;
+    }
 }
--- test/RabbitBreed.js
+++ test/RabbitBreed.js
@@ -8,14 +8,45 @@ chai.use(solidity);
 const { ethers, config } = require('hardhat');
 const { time } = require("@openzeppelin/test-helpers");
 const { toNum, toBN } = require("./utils/bignumberConverter");
+const { expect } = require('chai');
+
+const { parseEther } = ethers.utils;
 
 describe("RabbitBreed", function () {
   let owner, trader, trader1, trader2, trader3;
   let deployer;
   let rabbitBreed;
   let rabbitRocket;
-  //TODO: add tests
-  it("Should accept BNB deposits", async function () {
+  before(async function() {
+    [owner, trader, trader1, trader2, trader3] = await ethers.getSigners();
 
+    const RabbitRocket = await ethers.getContractFactory("RabbitRocket");
+    const startTime = (await time.latest()).toNumber();
+    rabbitRocket = await RabbitRocket.deploy(
+      startTime + 3600,//uint32 _startEpoch,
+      startTime + 3600 + 86400,//uint32 _whitelistEndEpoch,
+      86400//uint32 _countdownSeconds
+    );
+
+    const RabbitBreed = await ethers.getContractFactory("RabbitBreed");
+    rabbitBreed = await RabbitBreed.deploy(
+      rabbitRocket.address
+    );
+
+  });
+  it("Should accept BNB deposits and send reweards", async function () {
+    await time.increase(time.duration.days(5));
+    await rabbitBreed.addToRewards({value:parseEther("1")});
+    await rabbitBreed.sendRewards(
+      trader1.address,
+      trader2.address,
+      trader3.address,
+    );
+    const payments1 = await rabbitBreed.payments(trader1.address);
+    const payments2 = await rabbitBreed.payments(trader2.address);
+    const payments3 = await rabbitBreed.payments(trader3.address);
+    expect(payments1).to.eq(parseEther("0.5"));
+    expect(payments2).to.eq(parseEther("0.25"));
+    expect(payments3).to.eq(parseEther("0.25"));
   });
 });
